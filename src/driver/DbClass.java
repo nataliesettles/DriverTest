ac78f0bf9b7881de9c79d054c6d559a4d3e99780
Natalie Settles <studio@nataliesettles.com>
2013-05-16 11:55:13 -0400

    add files

---
+++ src/driver/DbClass.java
@@ -0,0 +1,61 @@
+package driver;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.ResultSetMetaData;
+import java.sql.DatabaseMetaData;
+
+public class DbClass {
+//	public String sUrl; // for advertising and debug purposes
+	public String sDbUrl;
+	private String sDriverName = "org.sqlite.JDBC";
+//	private String sDriver;
+	private Connection conn = null;
+	private Statement stmt = null;
+	private String sDriver = "jdbc:sqlite"; // moved this here, because as long
+	
+	
+	public DbClass(String sDatabaseToUse_) throws Exception {
+		String sDatabaseToUse = sDatabaseToUse_;
+		String sDbUrl = sDriver + ":" + sDatabaseToUse;
+//		sDriverName = getDriverString(sDbUrl); // this function will split the driver string from the Url
+		
+		setConnection();
+	}
+	
+	private void setConnection() throws Exception {
+		try {
+			Class.forName(sDriverName);
+		} catch (Exception e) {
+			// connection failed
+			System.out.println("DriverName: " + sDriver + " was not available");
+			System.err.println(e);
+			throw e;
+		}
+		// create database connection
+		conn = DriverManager.getConnection(sDbUrl);
+		try {
+			stmt = conn.createStatement();
+		} catch (Exception e) {
+			try { conn.close(); } catch (Exception ignore) {}
+			conn = null;
+		}
+	}
+	
+	// to close the database connection in the main program, call this method:
+	public void closeConnection() {
+		if (stmt != null) { try { stmt.close(); } catch (Exception ignore) {} }
+		if (conn != null) { try { conn.close(); } catch (Exception ignore) {} }
+	}
+	
+	public ResultSet executeQuery(String instruction) throws SQLException {
+		return stmt.executeQuery(instruction);
+	}
+	
+	public void execute(String instruction) throws SQLException {
+		stmt.executeUpdate(instruction);
+	}
+}
---
+++ src/driver/DriverConnection.java
@@ -0,0 +1,46 @@
+package driver;
+
+import java.sql.Connection;
+import java.sql.Statement;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.ResultSetMetaData;
+import java.sql.DatabaseMetaData;
+
+public class DriverConnection {
+
+
+
+public static void main (String[] args) throws Exception {
+
+	// the sql string commands
+	String sDropTable = "DROP TABLE IF EXISTS dummy"; // this deletes the table if it already exists
+	String sMakeTable = "CREATE TABLE dummy (id numeric, response text)";
+	String sMakeInsert = "INSERT INTO dummy VALUES (1,'Hello from the database')";
+	String sMakeSelect = "SELECT response from dummy";
+	
+	
+    String sDatabaseToUse = "hello.db";
+    
+	
+	DbClass db = new DbClass(sDatabaseToUse);
+	try {
+		db.execute(sDropTable);
+		db.execute(sMakeTable);
+		db.execute(sMakeInsert);
+		
+		ResultSet rs = db.executeQuery(sMakeSelect);
+	try {
+		while(rs.next()) {
+			String sResult = rs.getString("response");
+			System.out.println(sResult);
+		}
+	} finally {
+		try { rs.close(); } catch (Exception ignore) {}
+	}
+	} finally {
+		try { db.closeConnection(); } catch (Exception ignore) {}
+}
+}
+}
+
